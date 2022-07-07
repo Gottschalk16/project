e2ec51f92dfd329c5964eea67d074f114cf41179
Gottschalk16 <raianne_gottschalk@hotmail.com.br>
2022-07-07 19:36:32 -0300

    Otimizado select dos usuários para retorar em formato JSON

--- src/dados/Usuarios/getUsuarios.php
+++ src/dados/Usuarios/getUsuarios.php
@@ -1,5 +1,6 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json. charset=UTF-8");
 include_once 'ClassConexao.php';
 
 $query_usuarios = "SELECT id, ativo, usuario, senha FROM usuario ORDER BY id ASC";
@@ -8,7 +9,17 @@ $result_usuarios-> execute();
 
 if(($result_usuarios) AND ($result_usuarios->rowCount() != 0)){
   while($row_usuario = $result_usuarios->fetch(PDO::FETCH_ASSOC)){
-    var_dump($row_usuario);
+    extract($row_usuario);
+
+    $lista_usuarios["records"][$usuario] = [
+      'id' => $id,
+      'ativo' => $ativo,
+      'usuario' => $usuario,
+      'senha' => $senha
+    ];
+    http_response_code(200);
+
+    echo json_encode($lista_usuarios);
   }
 }
 ?>
